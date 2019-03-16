09c237856d32044ed43dc1fec022a88c6d34e758
Nujenova <nujenova@gmail.com>
2019-03-16 09:30:32 +0800

    Add files via upload

---
+++ smallestmissinginteger.py
@@ -0,0 +1,31 @@
+A = [0]
+B = len(A)
+
+def solution(A):
+    A.sort()
+    B = len(A)
+    mini = 0 
+    counter = 0
+    if max(A) < 0:
+        return 1
+    else:
+        for ele in A:
+            if ele <= mini:
+                counter += 1
+                if counter == B:
+                    return (mini+1)
+                    break
+                else:
+                    continue            
+            elif (ele - 1) == mini:
+                mini = ele
+                counter += 1
+                if counter == B:
+                    return (mini+1)
+                    break
+                else:
+                    continue
+            else:
+                return (mini+1)
+            
+print(solution(A))
